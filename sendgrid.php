a3906d3a97f566ab1098809be41f96174d972316
shilpa807 <46704160+shilpa807@users.noreply.github.com>
2019-02-22 12:59:53 +0530

    Update sendgrid.php

--- sendgrid.php
+++ sendgrid.php
@@ -1,4 +1,30 @@
 <?php
-echo "hello";
-echo "my edit changes";
+require("sendgrid-php/sendgrid-php.php");
+$fromEmail = "support@".$current_domain_name;
+$htmlContent = $message;
+$from = new SendGrid\Mail\From($fromEmail);
+$to = new SendGrid\Mail\To($to);
+$content = new SendGrid\Mail\Content("text/html", $htmlContent);
+$email = new SendGrid\Mail\Mail($from, $to, $subject);
+$email->addContent($content);
+
+$sg = new SendGrid('MY_SENDGRID_ID');
+try {
+    $response = $sg->client->mail()->send()->post($email);
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}        
+if( isset($response) ){
+    if($_SESSION['username']!='')
+    {
+        $sucess = "You are now logged in";
+        header("location: checkout.php");
+    }  
+    else 
+    {
+        header('Refresh: 1; url=login_register.php?action=joined');
+    }    
+}
+
+}
 ?>
